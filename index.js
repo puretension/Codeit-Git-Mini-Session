0800a011793a5e499b4505f7f0cad886d916ecd5
puretension <rlrlfhtm5@gmail.com>
2025-08-27 19:48:31 +0900

    stash push, pop

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import App from './src/App/App';
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+import App from "./src/App/App";
 
-function add(a,b) {return a + c + d + b}
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+function farewell(name) {
+  return `Goodbye, ${name}!`;
+}
+
+function add(a,b) {return a + a + b + c + d + b}
 
 AppRegistry.registerComponent(appName, () => App);
