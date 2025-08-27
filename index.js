b87328c5dbef33a6a3b54e0500068a47b55555e7
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2025-08-27 19:44:24 +0900

    Add add function for summing two numbers

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/App/App';
 
+function add(a,b) {return a + b}
+
 AppRegistry.registerComponent(appName, () => App);
